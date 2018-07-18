5d65962623c26f632f420db89e74bb3946e440d6
Dennis Majvall <dennis.majvall.sp13@thegameassembly.com>
2018-07-18 13:17:18 +0200

    test-event a little bit easier to read

--- frontend/js/components/test/test-events.js
+++ frontend/js/components/test/test-events.js
@@ -14,12 +14,15 @@ class TestEventsComponent extends Component {
   static template(){
     return `
       <span on-click="  this.sayHello1  ">
+        Click me (no args, with unnecessary "this")
+      </span><br>
+      <span on-click="  sayHello1  ">
         Click me (no args)
       </span><br>
-      <span on-click="  this.sayHello2('onlyText')  ">
+      <span on-click="  sayHello2('onlyText')  ">
         Click me (1 arg)
       </span><br>
-      <span on-click="  this.sayHello3('firstText', 'secondText')  ">
+      <span on-click="  sayHello3('firstText', 'secondText')  ">
         Click me (2 args + eventObject)
       </span>
     `;
